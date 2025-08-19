2e4780524f2f6f8092fa0f6cc4bdfae4722ab8bb
Douglas Lowder <douglowder@mac.com>
2025-08-18 17:36:53 -0700

    SFIcons for tab bar on iOS/tvOS

--- layouts/TabLayout.tsx
+++ layouts/TabLayout.tsx
@@ -5,21 +5,27 @@ export default function TabLayout() {
     <NativeTabs>
       <NativeTabs.Trigger name="index">
         <Label>Home</Label>
+        <Icon sf="house.fill" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="breathe">
         <Label>Breathe</Label>
+        <Icon sf="lungs.fill" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="glass">
         <Label>Glassmorphism</Label>
+        <Icon sf="window.vertical.open" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="reanimated">
         <Label>Reanimated</Label>
+        <Icon sf="atom" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="barchart">
         <Label>Bar Chart</Label>
+        <Icon sf="chart.bar.fill" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="linechart">
         <Label>Line Chart</Label>
+        <Icon sf="chart.line.uptrend.xyaxis" />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
