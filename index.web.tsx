3d9731630fb5692876dba5c5043150212675f357
Douglas Lowder <douglowder@mac.com>
2025-09-04 14:30:46 -0700

    SDK 54 preview 15, RNSkia 2.2.9

--- index.web.tsx
+++ index.web.tsx
@@ -5,7 +5,7 @@ import { LoadSkiaWeb } from '@shopify/react-native-skia/lib/module/web';
 import { version } from 'canvaskit-wasm/package.json';
 
 LoadSkiaWeb({
-  locateFile: (file) => `https://cdn.jsdelivr.net/npm/canvaskit-wasm@${version}/bin/full/${file}`
+//  locateFile: (file) => `https://cdn.jsdelivr.net/npm/canvaskit-wasm@${version}/bin/full/${file}`
 }).then(async () => {
   renderRootComponent(App);
 });
